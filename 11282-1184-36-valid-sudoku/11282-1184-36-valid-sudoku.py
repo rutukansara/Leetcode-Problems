0b97f49c256a8ac8d17abab23943aad2b38fe13c
Rutu Kansara <149906777+rutukansara@users.noreply.github.com>
2025-02-22 18:43:09 -0800

    Time: 4 ms, Memory: 18 MB - LeetHub

---
+++ 11282-1184-36-valid-sudoku/11282-1184-36-valid-sudoku.py
@@ -0,0 +1,39 @@
+# each row and col contains digits 1-9 without repeating
+# each box contains digits 1-9 without repeating
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # validating rows
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[i][j]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+
+        # validating columns
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[j][i]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+
+        # validating boxes
+        starts = [(0, 0), (0, 3), (0, 6),
+                (3, 0), (3, 3), (3, 6),
+                (6, 0), (6, 3), (6, 6)]
+
+        for i, j in starts:
+            s = set()
+            for row in range(i, i+3):
+                for col in range(j, j+3):
+                    item = board[row][col]
+                    if item in s:
+                        return False
+                    elif item != '.':
+                        s.add(item)
+        return True
